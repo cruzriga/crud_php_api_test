81c5a1a433a77b4342966c59308a3faa21aa4336
Cruz Rivera <cruz.riga@hotmail.com>
2021-02-27 23:47:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-include 'core/Config.php';
-include 'core/Constants.php';
-include 'core/MainController.php';
-include 'core/MainModel.php';
-include 'core/Router.php';
-
-$router = new Router();
-
+echo 'running php' ?> 
 
 
 
